12425bc5ce7024f8e6b98feda5798922c54e5ee5
yingming25 <karcnonf@gmail.com>
2018-12-28 12:28:16 +0800

    add actions and mutaions to get products

--- src/store/products.js
+++ src/store/products.js
@@ -1,7 +1,28 @@
+import axios from 'axios';
+
 export default {
   namespaced: true,
   state: {
     products: [],
     tempProduct: {},
+  },
+  actions: {
+    getProducts(context, page) {
+      const api = `${process.env.VUE_APP_API_PATH}/api/${
+        process.env.VUE_APP_CUSTOM_PATH
+      }/admin/products?page=${page}`;
+
+      context.dispatch('updateLoading', true, { root: true });
+
+      axios.get(api).then(response => {
+        context.commit('PRODUCTS', response.data.products);
+        context.dispatch('updateLoading', false, { root: true });
+      });
+    },
+  },
+  mutations: {
+    PRODUCTS(state, products) {
+      state.products = products;
+    },
   },
 };
