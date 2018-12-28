bbf4198744b610f011059de75c81ccb1dd0b82de
yingming25 <karcnonf@gmail.com>
2018-12-28 22:38:34 +0800

    add actions's method for sign in
    add actions's method for sign. import axios and router.

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,9 +1,23 @@
+import axios from 'axios';
+import router from '../router';
+
 export default {
   namespaced: true,
   state: {
     user: {
       username: '',
       password: '',
+    },
+  },
+  actions: {
+    signIn(context) {
+      const api = `${process.env.VUE_APP_API_PATH}/admin/signin`;
+      axios.post(api, context.state.user).then(response => {
+        // console.log(response.data);
+        if (response.data.success) {
+          router.push('/admin/products');
+        }
+      });
     },
   },
 };
