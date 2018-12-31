b0e388f94cf22bf70684910eb486814fbd34143f
yingming25 <karcnonf@gmail.com>
2018-12-31 15:21:41 +0800

    import orders module

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import productsModule from './products';
 import authModule from './auth';
+import ordersModule from './orders';
 
 Vue.use(Vuex);
 
@@ -28,5 +29,6 @@ export default new Vuex.Store({
   modules: {
     productsModule,
     authModule,
+    ordersModule,
   },
 });
