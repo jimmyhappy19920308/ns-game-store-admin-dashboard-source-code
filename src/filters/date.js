f42372a0f958538ae5b15c8ccd943c2ab31489a2
yingming25 <karcnonf@gmail.com>
2019-01-01 09:55:18 +0800

    update date format in less than ten condition

--- src/filters/date.js
+++ src/filters/date.js
@@ -1,8 +1,16 @@
 export default function(time) {
   const dates = new Date(time * 1000);
   const year = dates.getFullYear();
-  const month = dates.getMonth() + 1;
-  const date = dates.getDate();
+  let month = dates.getMonth() + 1;
+  let date = dates.getDate();
+
+  if (month < 10) {
+    month = `0${month}`;
+  }
+
+  if (date < 10) {
+    date = `0${date}`;
+  }
   return `${year}/${month}/${date}`;
   // return dates.toLocaleDateString();
 }
