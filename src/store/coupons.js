b96a54d337acd5c8bd713255619eda0d83185134
yingming25 <karcnonf@gmail.com>
2019-01-01 10:13:23 +0800

    add coupons module

---
+++ src/store/coupons.js
@@ -0,0 +1,15 @@
+export default {
+  namespaced: true,
+  state: {
+    coupons: [],
+    isNew: false,
+    isLoading: false,
+    tempCoupon: {
+      title: '',
+      is_enabled: false,
+      percent: 100,
+      due_date: 0,
+      code: '',
+    },
+  },
+};
