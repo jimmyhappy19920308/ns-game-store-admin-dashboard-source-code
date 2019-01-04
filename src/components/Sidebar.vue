e48929b4ce11b0ecf415f4751a9d664d6a2a8e33
yingming25 <karcnonf@gmail.com>
2019-01-04 15:51:06 +0800

    update text

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,7 +3,7 @@
     <nav class="col-md-2 d-none d-md-block bg-light sidebar">
       <div class="sidebar-sticky">
         <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          <span>Saved reports</span>
+          <span>相關列表</span>
           <a class="d-flex align-items-center text-muted" href="#">
             <span data-feather="plus-circle"></span>
           </a>
